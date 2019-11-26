99a03c13009bd96085996fc58b8a2f951b77981e
Gilberto Masetto <gilberto.masetto@gmail.com>
2019-11-25 22:41:46 -0300

    Initial Commit

---
+++ mysignos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mysignos
+//
+//  Created by Gilberto Masetto on 25/11/19.
+//  Copyright © 2019 Gilberto Masetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
